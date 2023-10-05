6e152ae57404a2f882ae73e9638cb620e464535d
Kirmen <106472439+Kirmen@users.noreply.github.com>
2023-10-05 23:54:43 +0300

    make Models, setting Admin panel, add DjDT, add CKEditor

--- SiteBlog/blog/admin.py
+++ SiteBlog/blog/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import *
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('id', 'title', 'slug')
+    list_display_links = ('id', 'title')
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = _fields = {'slug': ('title',)}
+    list_display = ('id', 'title', 'slug')
+    list_display_links = ('id', 'title')
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    form = PostAdminForm
+    save_on_top = True
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_filter = ('category',)
+    readonly_fields = ('views',)  # можна get_photo та created_at, але вони й так не виводяться
+
+    # save_as = True зберігти як новий об'єкт
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50" >')
+        return '-'
+
+    get_photo.short_description = 'Фото'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
--- SiteBlog/blog/models.py
+++ SiteBlog/blog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255,
+                             verbose_name='Категорія')
+    slug = models.SlugField(max_length=255,
+                            verbose_name='Url',
+                            unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Категорії'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50,
+                             verbose_name='Тег')
+    slug = models.SlugField(max_length=50,
+                            verbose_name='Url',
+                            unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Теги'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=50,
+                            verbose_name='Url',
+                            unique=True)
+    author = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубліковано')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    views = models.IntegerField(default=0, verbose_name='Кількість переглядів')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='post')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name_plural = 'Пости'
