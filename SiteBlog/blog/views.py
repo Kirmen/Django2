ee3ec61a603beae858b82f2f74a5c6aa039b1629
Kirmen <106472439+Kirmen@users.noreply.github.com>
2023-10-12 23:08:14 +0300

    make pagination, show posts by category, show single post

--- SiteBlog/blog/views.py
+++ SiteBlog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.db.models import F
 
 from blog.models import Post, Category, Tag
 
@@ -12,17 +13,43 @@ class Home(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'kirmen\'s blog'
+        context['title'] = 'Kirmen'
         return context
 
 
+class PostByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+    allow_empty = False
 
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
+
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
+
+class PostByTag(ListView):
+    pass
 # def index(request):
 #     return render(request, 'blog/index.html')
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
+# def get_category(request, slug):
+#     return render(request, 'blog/category.html')
 
-def get_post(request, slug):
-    return render(request, 'blog/post.html')
+# def get_post(request, slug):
+#     return render(request, 'blog/post.html')
